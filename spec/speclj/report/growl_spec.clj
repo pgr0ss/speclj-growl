b5639091aa795e137907099fce65f00a19daee61
micah and paul <pgross@gmail.com>
2011-09-22 13:35:01 -0500

    Started working on growling nice messages.

--- spec/speclj/report/growl_spec.clj
+++ spec/speclj/report/growl_spec.clj
@@ -1,12 +1,37 @@
 (ns speclj.report.growl_spec
   (:use
     [speclj.core]
-    [speclj.report.growl]))
+    [speclj.exec :only (pass-result fail-result pending-result)]
+    [speclj.report.growl]
+    [speclj.reporting :only (report-runs)]))
 
 
 (describe "Growl Reporter"
-  (it "blah"
-      (pending)
-      )
 
-          )
+  (with reporter (new-growl-reporter))
+  (with notification (atom nil))
+  (with title (atom nil))
+  (with message (atom nil))
+  (with fake-growl (fn [_notification _title _message]
+                     (reset! @notification _notification)
+                     (reset! @title _title)
+                     (reset! @message _message)))
+
+
+  (it "growls empty test summary"
+      (binding [growl @fake-growl]
+        (report-runs @reporter [])
+        (should= "Success" @@notification)
+        (should= "No Specs" @@title)
+        (should= "No Specs" @@message)))
+
+  (it "growls a successful run"
+      (pending)
+      (binding [growl @fake-growl]
+        (let [result1 (pass-result nil 0.1)
+              result2 (pass-result nil 0.02)
+              results [result1 result2]]
+          (report-runs @reporter results)
+          (should= "Success" @@notification)
+          (should= "Success" @@title)
+          (should= "2 examples, 0 failures" @@message)))))
--- src/speclj/report/growl.clj
+++ src/speclj/report/growl.clj
@@ -1,20 +1,24 @@
 (ns speclj.report.growl
   (:use
     [speclj.reporting :only ()]
+    [speclj.report.progress :only (print-summary)]
     [clj-growl.core :only (make-growler)])
   (:import
     [speclj.reporting Reporter]))
 
-(def growl (make-growler "" "speclj" ["Message" true]))
+(def growl (make-growler "" "speclj" ["Success" true "Failure" true]))
 
 (deftype GrowlReporter [passes fails results]
     Reporter
     (report-message [this message])
     (report-description [this description])
     (report-pass [this result])
-    (report-pending [this result] (growl "Message" "Specs" (str result)))
+    (report-pending [this result])
     (report-fail [this result])
-    (report-runs [this results]))
+    (report-runs [this results]
+      (if (seq results)
+        (growl "Success" "Success" (with-out-str (print-summary results)))
+        (growl "Success" "No Specs" "No Specs"))))
 
 (defn new-growl-reporter []
     (GrowlReporter. (atom 0) (atom 0) (atom nil)))
