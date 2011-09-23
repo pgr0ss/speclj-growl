a57f6f448a6e1918f2c31e578f50b3cef163c170
Paul Gross <pgross@gmail.com>
2011-09-23 12:02:13 -0500

    Print and growl a summary message when specs complete.

--- spec/speclj/report/growl_spec.clj
+++ spec/speclj/report/growl_spec.clj
@@ -7,7 +7,6 @@
 
 
 (describe "Growl Reporter"
-
   (with reporter (new-growl-reporter))
   (with notification (atom nil))
   (with title (atom nil))
@@ -17,21 +16,25 @@
                      (reset! @title _title)
                      (reset! @message _message)))
 
+  (describe "report-runs"
+    (it "prints summary to the terminal"
+        (binding [growl @fake-growl]
+          (let [output (with-out-str (report-runs @reporter []))]
+            (should (.contains output "0 examples, 0 failures")))))
 
-  (it "growls empty test summary"
-      (binding [growl @fake-growl]
-        (report-runs @reporter [])
-        (should= "Success" @@notification)
-        (should= "No Specs" @@title)
-        (should= "No Specs" @@message)))
+    (it "growls summary information for no test runs"
+        (binding [growl @fake-growl]
+          (let [output (with-out-str (report-runs @reporter []))]
+            (should= "Message" @@notification)
+            (should= "Specs" @@title)
+            (should= "0 examples, 0 failures" @@message))))
 
-  (it "growls a successful run"
-      (pending)
-      (binding [growl @fake-growl]
-        (let [result1 (pass-result nil 0.1)
-              result2 (pass-result nil 0.02)
-              results [result1 result2]]
-          (report-runs @reporter results)
-          (should= "Success" @@notification)
-          (should= "Success" @@title)
-          (should= "2 examples, 0 failures" @@message)))))
+    (it "growls a successful run"
+        (binding [growl @fake-growl]
+          (let [result1 (pass-result nil 0.1)
+                result2 (pass-result nil 0.02)
+                results [result1 result2]
+                output (with-out-str (report-runs @reporter results))]
+            (should= "Message" @@notification)
+            (should= "Specs" @@title)
+            (should= "2 examples, 0 failures" @@message))))))
--- src/speclj/report/growl.clj
+++ src/speclj/report/growl.clj
@@ -1,12 +1,35 @@
 (ns speclj.report.growl
   (:use
+    [speclj.exec :only (pass? fail? pending?)]
     [speclj.reporting :only ()]
     [speclj.report.progress :only (print-summary)]
     [clj-growl.core :only (make-growler)])
   (:import
     [speclj.reporting Reporter]))
 
-(def growl (make-growler "" "speclj" ["Success" true "Failure" true]))
+(def growl (make-growler "" "speclj" ["Message" true]))
+
+(defn- categorize [results]
+  (reduce (fn [tally result]
+    (cond (pending? result) (update-in tally [:pending] conj result)
+      (fail? result) (update-in tally [:fail] conj result)
+      :else (update-in tally [:pass] conj result)))
+    {:pending [] :fail [] :pass []}
+    results))
+
+(defn- describe-counts-for [result-map]
+  (let [tally (apply hash-map (interleave (keys result-map) (map count (vals result-map))))
+        always-on-counts [(str (apply + (vals tally)) " examples")
+                          (str (:fail tally) " failures")]]
+    (apply str
+      (interpose ", "
+        (if (> (:pending tally) 0)
+          (conj always-on-counts (str (:pending tally) " pending"))
+          always-on-counts)))))
+
+(defn growl-message [results]
+  (let [result-map (categorize results)]
+    (describe-counts-for result-map)))
 
 (deftype GrowlReporter [passes fails results]
     Reporter
@@ -16,9 +39,8 @@
     (report-pending [this result])
     (report-fail [this result])
     (report-runs [this results]
-      (if (seq results)
-        (growl "Success" "Success" (with-out-str (print-summary results)))
-        (growl "Success" "No Specs" "No Specs"))))
+      (print-summary results)
+      (growl "Message" "Specs" (growl-message results))))
 
 (defn new-growl-reporter []
     (GrowlReporter. (atom 0) (atom 0) (atom nil)))
