4b5b0169d62da7c930400a5a4763ad9cbc43c6b4
Paul Gross <pgross@gmail.com>
2011-10-03 20:55:46 -0500

    Upgraded speclj.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Growl reporter for the speclj testing framework"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [clj-growl "0.2.1"]]
-  :dev-dependencies [[speclj "1.5.0"]]
+  :dev-dependencies [[speclj "1.5.1"]]
   :test-path "spec/")
