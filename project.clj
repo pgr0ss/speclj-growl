b6a8ab1c15ef61ac19422f6a7352f41a84c5de64
Paul Gross <pgross@gmail.com>
2011-12-01 14:32:46 -0600

    Release version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speclj-growl "1.0.0-SNAPSHOT"
+(defproject speclj-growl "1.0.0"
   :description "Growl reporter for the speclj testing framework"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [clj-growl "0.2.1"]]
