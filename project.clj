4b0768d017a91552df478164b55ae1f0cc20ef9b
Paul Gross <pgross@gmail.com>
2013-04-01 20:58:32 -0700

    Add url and license to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [gntp "0.6.0"]]
   :resource-paths ["resources"]
+  :url "https://github.com/pgr0ss/speclj-growl"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :profiles {
     :dev {
       :plugins [[speclj "2.5.0"]]
